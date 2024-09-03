36bc02ac71647461d997bce8f02155c9695744ca
imrankhan <imrankhaninteract@gmail.com>
2024-09-03 19:35:01 +0600

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 
+
 import Nice from './Nice.jsx'
 import Nav from './assets/components/Navbar/Nav.jsx'
+import Header from './assets/components/Header/Header.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    
-    <Nice />
-    <Nav />
     
+    <Header />
     
 
   
