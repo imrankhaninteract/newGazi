fd09bba151071de4861b27815e7197c2ec39397c
imrankhan <imrankhaninteract@gmail.com>
2024-09-06 16:02:44 +0600

    commit

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import './Header.css';
 
+
+
 const Header = () => {
   const [isNavbarActive, setNavbarActive] = useState(false);
   const [isHeaderSticky, setHeaderSticky] = useState(false);
@@ -50,57 +52,115 @@ const Header = () => {
   }, []);
 
   return (
-    <header className={`header ${isHeaderSticky ? "active" : ""}`} data-header>
-      <div className="container">
-        <h1 className="h1 logo">
-          <a href="#">Gazi<span>.</span></a>
-        </h1>
+    <>
+      <header className={`header ${isHeaderSticky ? "active" : ""}`} data-header>
+        <div className="container">
+          <h1 className="h1 logo">
+            <a href="#">Gazi<span>.</span></a>
+          </h1>
 
-        <div className="navbar-actions">
+          <div className="navbar-actions">
+            <button
+              className={`theme-btn ${!isThemeDark ? "active" : ""}`} // Button is active when it's light
+              aria-label="Change Theme"
+              title="Change Theme"
+              onClick={handleThemeToggle}
+              data-theme-btn
+            >
+              <span className="icon"></span>
+            </button>
+          </div>
+
           <button
-            className={`theme-btn ${!isThemeDark ? "active" : ""}`} // Button is active when it's light
-            aria-label="Change Theme"
-            title="Change Theme"
-            onClick={handleThemeToggle}
-            data-theme-btn
+            className="nav-toggle-btn"
+            aria-label="Toggle Menu"
+            title="Toggle Menu"
+            onClick={() => toggleElement(setNavbarActive)}
+            data-nav-toggle-btn
           >
-            <span className="icon"></span>
+            <span className="one"></span>
+            <span className="two"></span>
+            <span className="three"></span>
           </button>
+
+          <nav className={`navbar ${isNavbarActive ? "active" : ""}`} data-navbar>
+            <ul className="navbar-list">
+              <li>
+                <a href="#home" className="navbar-link">Home.</a>
+              </li>
+              <li>
+                <a href="#about" className="navbar-link">About.</a>
+              </li>
+              <li>
+                <a href="#skills" className="navbar-link">Skills.</a>
+              </li>
+              <li>
+                <a href="#portfolio" className="navbar-link">Portfolio.</a>
+              </li>
+              <li>
+                <a href="#contact" className="navbar-link">Contact.</a>
+              </li>
+            </ul>
+          </nav>
         </div>
+      </header>
 
-        <button
-          className="nav-toggle-btn"
-          aria-label="Toggle Menu"
-          title="Toggle Menu"
-          onClick={() => toggleElement(setNavbarActive)}
-          data-nav-toggle-btn
-        >
-          <span className="one"></span>
-          <span className="two"></span>
-          <span className="three"></span>
-        </button>
+      <main>
+        <article className="container">
+          <section className="hero" id="home">
+            <figure className="hero-banner">
+              <picture>
+                <source
+                  srcSet="./assets/images/hero-banner.png"
+                  media="(min-width: 768px)"
+                />
+                <source
+                  srcSet="./assets/images/hero-banner.png"
+                  media="(min-width: 500px)"
+                />
+                <img
+                  src="./assets/images/hero-banner.png"
+                  alt="A man in a blue shirt with a happy expression"
+                  className="w-100"
+                />
+              </picture>
+            </figure>
 
-        <nav className={`navbar ${isNavbarActive ? "active" : ""}`} data-navbar>
-          <ul className="navbar-list">
-            <li>
-              <a href="#home" className="navbar-link">Home.</a>
-            </li>
-            <li>
-              <a href="#about" className="navbar-link">About.</a>
-            </li>
-            <li>
-              <a href="#skills" className="navbar-link">Skills.</a>
-            </li>
-            <li>
-              <a href="#portfolio" className="navbar-link">Portfolio.</a>
-            </li>
-            <li>
-              <a href="#contact" className="navbar-link">Contact.</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+            <div className="hero-content">
+              <h2 className="h2 hero-title">Gazi Interior Design</h2>
+              <a href="#contact" className="btn btn-primary">
+                Get in touch
+              </a>
+            </div>
+
+            <ul className="hero-social-list">
+              <li>
+                <a href="#" className="hero-social-link">
+                  <ion-icon name="logo-facebook"></ion-icon>
+                  <div className="tooltip">Facebook</div>
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hero-social-link">
+                  <ion-icon name="logo-twitter"></ion-icon>
+                  <div className="tooltip">Twitter</div>
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hero-social-link">
+                  <ion-icon name="logo-linkedin"></ion-icon>
+                  <div className="tooltip">LinkedIn</div>
+                </a>
+              </li>
+            </ul>
+
+            <a href="#stats" className="scroll-down">
+              Scroll
+            </a>
+          </section>
+        </article>
+      </main>
+    </>
   );
 };
 
